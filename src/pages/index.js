9cb38049cb6cf443efedd678413dfcbb7afc46e7
Matt <matt.mullis07@gmail.com>
2022-05-29 14:18:08 -0700

    Finished Gatsby tutorial part 3 adding plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby tutorial.</p>
+      <StaticImage
+        alt="Gatsby and React logos"
+        src="../images/gatsbyandreactlogos.png"
+      />
     </Layout>
   );
 };
